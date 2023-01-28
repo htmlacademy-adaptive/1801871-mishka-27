769cc666c000e759cfb8f1a0592abad0ea6d50a8
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-01-28 13:11:14 +0300

    Правки2

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
 let navHeader = document.querySelector('.header-navigation');
 let navToggle = document.querySelector('.header-navigation__burger');
-console.log(navHeader);
+
 navHeader.classList.remove('header-navigation--no-js');
 navHeader.classList.add('header-navigation--closed');
-console.log("11");
+
 
 navToggle.addEventListener('click', function () {
   if (navHeader.classList.contains('header-navigation--closed')) {
